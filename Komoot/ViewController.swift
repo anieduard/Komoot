ed82419bf1c387dd83e6e9dc81aee530842d2066
Ani Eduard <eduard@halcyonmobile.com>
2019-10-21 22:49:36 +0300

    Initial Commit

---
+++ Komoot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Komoot
+//
+//  Created by Ani Eduard on 21/10/2019.
+//  Copyright © 2019 Ani Eduard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

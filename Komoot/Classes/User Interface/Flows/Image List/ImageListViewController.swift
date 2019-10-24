e8c77b97b81c814d60f0bdf8e2fc4192f31b1c43
Ani Eduard <eduard@halcyonmobile.com>
2019-10-24 20:08:10 +0300

    Implement UI for table view and start / stop buttons.

--- Komoot/Classes/User Interface/Flows/Image List/ImageListViewController.swift
+++ Komoot/Classes/User Interface/Flows/Image List/ImageListViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class ImageListViewController: UIViewController {
     
+    // MARK: - Private properties
+    
     private let viewModel: ImageListViewModel
+    private let tableView = UITableView()
+    
+    private lazy var dataSource: UITableViewDiffableDataSource<Section, Photo> = {
+        UITableViewDiffableDataSource<Section, Photo>(tableView: tableView) { tableView, indexPath, photo in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = photo.name
+            return cell
+        }
+    }()
+    
+    // MARK: - Init
     
     init(viewModel: ImageListViewModel) {
         self.viewModel = viewModel
@@ -21,8 +34,65 @@ class ImageListViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - UI
+    
+    override func loadView() {
+        view = tableView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: .start, style: .plain, target: self, action: #selector(rightBarButtonItemTouched))
+        
+        tableView.dataSource = dataSource
+        tableView.separatorStyle = .none
+        tableView.tableFooterView = UIView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Photo>()
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems([Photo(name: "a"), Photo(name: "b"), Photo(name: "c")], toSection: .photos)
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
+    
+    // MARK: - Bindings
+    
+    // MARK: - User interaction
+    
+    @objc private func rightBarButtonItemTouched(_ sender: UIBarButtonItem) {
+        if sender.title == .start {
+            sender.title = .stop
+            startButtonTouched()
+        } else if sender.title == .stop {
+            sender.title = .start
+            stopButtonTouched()
+        }
+    }
+    
+    private func startButtonTouched() {
+        
+    }
+    
+    private func stopButtonTouched() {
+        
+    }
+}
+
+private extension ImageListViewController {
+    
+    enum Section: CaseIterable {
+        case photos
+    }
+
+    struct Photo: Hashable {
+        let name: String
+    }
+}
+
+// MARK: - Constants
+
+private extension String {
+    static let start = "Start"
+    static let stop = "Stop"
 }
